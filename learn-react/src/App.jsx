67a71b7a4a6bf0882b30fef1d7f8549376af8de1
Nirmal Dahal <nirmaldahal19@gmail.com>
2026-08-26 12:29:29 +0545

    props

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,16 +1,33 @@
-//import Student from "./Student";
-//import User from "./user";
+import Student from "./Student";
+import UserInfo from "./Userinfo";
+import User from "./Userinfo";
 
 function App(){
-  function greet(name){
-    console.log("hello " + name);
-  }
-  return(
-    <div>
-     <h1>react events</h1>
-     <button onClick={()=>greet("Nirmal")}>
-      Click me</button>
-      </div>
-  );
+ return(
+  <div>
+    <h1>my  react app details:</h1>
+    <User
+    id={0} 
+    name="nirmal"
+    role="react native developer"
+    /><br></br>
+
+    <User
+    id={1}
+    name="john"
+    role="backend"/>
+    
+
+  <br>
+  </br>
+  <User
+  id={2}
+  name="taran"
+  role="marketing"/><br></br>
+  <Student/>
+  </div>
+
+
+ );
 }
-export default App;
+ export default App;
---
+++ learn-react/src/Userinfo.jsx
@@ -0,0 +1,12 @@
+function UserInfo(props){
+            return(
+                <div>
+                    <p>Id:{props.id}</p>
+                    <p>Name: {props.name}</p>
+                    <p>Role: {props.role}</p>
+                    </div>
+            );
+        };
+
+
+export default UserInfo;
