8d1ab02e608b9e43bdb9c2578fbd15d85fddd60c
Nirmal Dahal <nirmaldahal19@gmail.com>
2026-08-27 13:34:12 +0545

    eshop

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-const students=[
-    {name:"nirmal", age:22, marks:45},
-    {name:"nirmala", age:21, marks:55},
-    {name:"nims", age:2, marks:95}
-];
-//forEach
-students.forEach(student=>{
-    console.log(student.name);
-});
-//filter
-const passed=students.filter(student=>student.marks >= 45);
-console.log(passed);
+const users=[
+  {
+    id:1,
+    name:"nirmal",
+   age:17
+  },
+{
+    id:2,
+    name:"john",
+    age:23
+}];
 //map
-const studentNames = students.map(student => student.name);
-console.log(studentNames);
-//filter+map
-const adultNames= students.filter(student=>student.age>=18)
-.map(student=>student.name);
-console.log(adultNames);
-//top
-const topstd= students.filter(student=>student.marks >= 80)
-.map(student=>student.name);
-console.log("topper is:" + topstd);
+const result= users.map(user=>user.name);
+console.log(result);
+//filter
+const age=users.filter(user=>user.age>18);
+console.log(age);
+//destructuring
+const { id } = users[1];
+console.log(id);
